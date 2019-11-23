5178e0756712061cdf74322b87298053445216ae
huyiqun753 <56007804+huyiqun753@users.noreply.github.com>
2019-11-22 21:10:36 -0800

    controller1.0

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,7 +8,7 @@ public class Player : MonoBehaviour
 
     public float moveSpeed = 10f;
     public float dashSpeed = 100f;
-    public float turnSpeed = 75f;
+    public float turnSpeed = 500000;
 
 
     // Start is called before the first frame update
@@ -29,9 +29,53 @@ public class Player : MonoBehaviour
 
     private void inputHandler()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.JoystickButton0))
         {
             moveForward();
+        }
+        if (Input.GetAxis("Horizontal_L") > 0.19f)
+        {
+            float angle = (transform.localEulerAngles.y % 360 + 360) % 360;
+            if (Mathf.Abs(angle - 90) > 3)
+            {
+                if (angle >= 90 && angle < 270)
+                    transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+                else transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+            }
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetAxis("Horizontal_L") < -0.19f)
+        {
+            float angle = (transform.localEulerAngles.y % 360 + 360) % 360;
+            if (Mathf.Abs(angle - 270) > 3)
+            {
+                if (angle >= 90 && angle < 270)
+                    transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+                else transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+            }
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetAxis("Vertical_L") > 0.19f)
+        {
+            float angle = transform.localEulerAngles.y%360;
+            if (Mathf.Abs(angle) > 3)
+            {
+                if (angle >= 0 && angle < 180)
+                    transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+                else transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+            }
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetAxis("Vertical_L") <-0.19f)
+        {
+            float angle = transform.localEulerAngles.y%360;
+            if (Mathf.Abs(angle-180) > 3)
+            {
+                if (angle >= 0 && angle < 180)
+                    transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+                else transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+            }
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.W))
