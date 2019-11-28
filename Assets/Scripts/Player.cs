d5e125092c0b3d84a1152c08c0f530ccd8f3d553
huyiqun753 <56007804+huyiqun753@users.noreply.github.com>
2019-11-27 20:00:11 -0800

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,26 +5,34 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     Rigidbody rigidBody;
-
-    public float moveSpeed = 100f;
-    public float dashForce = 100f;
+    public enum playerStates {MOVING, DASHING};
+    public float moveSpeed = 10f;
+    public float dashForce = 500f;
     public float turnSpeed = 500000;
-
+    public float dashTime = 0.5f;
+    public float dashCD = 0.1f;
+    public playerStates state;
+    
+    float dashTimer;
+    float dashCDcount;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
         rigidBody.freezeRotation = true;
+        dashTimer = 0.0f;
+        playerStates state = playerStates.MOVING;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         // Target Phase
-
-        inputHandler();
-
+        if (state == playerStates.MOVING)
+            inputHandler();
+        if (state == playerStates.DASHING)
+            dashForward();
     }
 
     float get_angle(float x, float y)
@@ -41,37 +49,52 @@ public class Player : MonoBehaviour
 
     private void inputHandler()
     {
-        if (Input.GetKey(KeyCode.JoystickButton0))
-        {
-            moveForward();
-        }
-        if (Mathf.Abs(Input.GetAxis("Horizontal_L")) > 0.19f||Mathf.Abs(Input.GetAxis("Vertical_L"))>0.19f)
-        {
-            float x = Input.GetAxis("Horizontal_L"),y= Input.GetAxis("Vertical_L");
+        if (dashCDcount >= 0.0f)
+            dashCDcount -= Time.deltaTime;
 
-            float angle = get_angle(x, y), currentAngle = (transform.localEulerAngles.y % 360 + 360) % 360; ;
-            transform.Rotate(Vector3.up, angle-currentAngle);
-            //rigidBody.AddForce(transform.forward * moveSpeed);
-            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
-        }
-        
+        if(dashCDcount<=0.0f)
+            if (Input.GetKey(KeyCode.JoystickButton0))
+            {
+                dashTimer = dashTime;
+                state = playerStates.DASHING;
+            }
+            if (Mathf.Abs(Input.GetAxis("Horizontal_L")) > 0.19f || Mathf.Abs(Input.GetAxis("Vertical_L")) > 0.19f)
+            {
+                float x = Input.GetAxis("Horizontal_L"), y = Input.GetAxis("Vertical_L");
 
-        if (Input.GetKey(KeyCode.W))
-            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+                float angle = get_angle(x, y), currentAngle = (transform.localEulerAngles.y % 360 + 360) % 360; ;
+                transform.Rotate(Vector3.up, angle - currentAngle);
+                //rigidBody.AddForce(transform.forward * moveSpeed);
+                transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+            }
 
-        if (Input.GetKey(KeyCode.S))
-            transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
 
-        if (Input.GetKey(KeyCode.A))
-            transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+            if (Input.GetKey(KeyCode.W))
+                transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
 
-        if (Input.GetKey(KeyCode.D))
-            transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+            if (Input.GetKey(KeyCode.S))
+                transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+
+            if (Input.GetKey(KeyCode.A))
+                transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+
+            if (Input.GetKey(KeyCode.D))
+                transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
 
     }
 
-    private void moveForward()
+    private void dashForward()
     {
-        rigidBody.AddForce(transform.forward * dashForce);
+        if (dashTimer >= dashTime-0.1f)
+            rigidBody.AddForce(transform.forward * dashForce);
+        else rigidBody.AddForce(transform.forward * 50.0f);
+        if (dashTimer <= 0.0f)
+        {
+            state = playerStates.MOVING;
+            dashCDcount = dashCD;
+            //rigidBody.AddForce(transform.forward * -dashForce);
+        }
+        dashTimer -= Time.deltaTime;
+        
     }
 }
