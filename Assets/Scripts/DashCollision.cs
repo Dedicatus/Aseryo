1224170b9cf4b3526179c057b25cd4e9906a436d
Yiming Zhang <allenzym@gmail.com>
2019-11-27 22:55:02 -0800

    update player inputHandler

--- Assets/Scripts/DashCollision.cs
+++ Assets/Scripts/DashCollision.cs
@@ -1,24 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Player;
 
 public class DashCollision : MonoBehaviour
 {
+    public GameObject parent;
+    Player mPlayer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mPlayer = parent.GetComponent<Player>();
+
+        Debug.Log("My state is:" + mPlayer.state);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        mPlayer = parent.GetComponent<Player>();
+
+        Debug.Log("My state is:" + mPlayer.state);
+        //gameObject.transform.parent.GetComponent(Player).state =
+        //Debug.Log(gameObject.transform.parent.GetComponent(Player).state);
     }
 
     private void OnTriggerEnter(Collider coll)
     {
-        if (coll.gameObject.tag == "Enemy")
+        if (coll.gameObject.tag == "Enemy" && mPlayer.state == playerStates.DASHING)
         {
             Destroy(coll.gameObject);
         }
