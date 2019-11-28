10e85525f456d2a751fd692e9bbe7b0f13ecde0e
Yiming Zhang <allenzym@gmail.com>
2019-11-28 02:14:18 -0800

    reconstruct player.prefab

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -35,8 +35,8 @@ public class Player : MonoBehaviour
         rigidBody = GetComponent<Rigidbody>();
         rigidBody.freezeRotation = true;
         playerCollider = GetComponent<BoxCollider>();
-        dashCollider = transform.FindChild("DashCollider").gameObject.GetComponent<BoxCollider>();
-        exploseColliderObject = transform.FindChild("ExploseCollider").gameObject;
+        dashCollider = transform.FindChild("Colliders").gameObject.transform.FindChild("DashCollider").gameObject.GetComponent<BoxCollider>();
+        exploseColliderObject = transform.FindChild("Colliders").gameObject.transform.FindChild("ExploseCollider").gameObject;
         dashTimer = 0.0f;
         playerStates state = playerStates.MOVING;
         isDashed = false;
