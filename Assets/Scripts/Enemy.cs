2efe84a815559e10296505e57534220b455d5e78
huyiqun753 <56007804+huyiqun753@users.noreply.github.com>
2019-12-01 19:44:03 -0800

    characters figures

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,7 +6,10 @@ public class Enemy : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public Transform target;
-
+    public GameObject player;
+    public float playerAttack;
+    public float Attack = 1f;
+    public float Health = 2f;
     public Enemy()
     {
         
@@ -15,7 +18,9 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        target = GameObject.FindGameObjectWithTag("Player").transform;
+        player = GameObject.FindGameObjectWithTag("Player");
+        target = player.transform;
+        playerAttack = player.GetComponent<Player>().Attack;
     }
 
     // Update is called once per frame
@@ -26,10 +31,20 @@ public class Enemy : MonoBehaviour
 
     void findPlayer()
     {
+        if(!player.GetComponent<Player>().isCollision())
+            Physics.IgnoreCollision(player.GetComponent<Collider>(), GetComponent<Collider>());
         Vector3 relativePos = target.position - transform.position;
         Quaternion rotation = Quaternion.LookRotation(relativePos, Vector3.up);
         transform.rotation = rotation;
         transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+    }
+
+    public bool getDestoried()
+    {
+        Health -= playerAttack;
+        if (Health <= 0)
+            return true;
+        return false;
     }
 
 }
