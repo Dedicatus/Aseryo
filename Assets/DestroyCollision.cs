741785678b84c98bbeb46c3de7c4ea2ad610286d
Yiming Zhang <allenzym@gmail.com>
2019-12-10 20:06:48 -0800

    destroy collision ground blood

--- Assets/DestroyCollision.cs
+++ Assets/DestroyCollision.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class DestroyCollision : MonoBehaviour
 {
+    FaintEffect faintEffect;
+
     private void OnTriggerEnter(Collider coll)
     {
         if (coll.gameObject.tag == "Enemy")
         {
+            faintEffect = coll.transform.GetComponent<FaintEffect>();
+            faintEffect.startGroundBlood();
             Destroy(coll.gameObject);
         }
 
