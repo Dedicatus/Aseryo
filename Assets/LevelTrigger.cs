44a2940b3945b6217f6dea363d4346afcd2020d1
Yiming Zhang <allenzym@gmail.com>
2019-12-11 08:02:20 -0800

    update levelTrigger

--- Assets/LevelTrigger.cs
+++ Assets/LevelTrigger.cs
@@ -13,8 +13,10 @@ public class LevelTrigger : MonoBehaviour
 
         if (!activated)
         {
-            transform.Find("Torch").gameObject.SetActive(false);
-            gameObject.GetComponent<BoxCollider>().enabled = false;
+            transform.parent.Find("Torch1").gameObject.SetActive(false);
+            transform.parent.Find("Torch2").gameObject.SetActive(false);
+            transform.parent.Find("Steam").gameObject.SetActive(false);
+            transform.GetComponent<BoxCollider>().enabled = false;
         }
     }
 
@@ -22,7 +24,10 @@ public class LevelTrigger : MonoBehaviour
     {
         if (gameObject.GetComponent<BoxCollider>().enabled == true)
         {
-            transform.Find("Torch").gameObject.SetActive(true);
+            transform.GetComponent<BoxCollider>().enabled = true;
+            transform.parent.Find("Torch1").gameObject.SetActive(true);
+            transform.parent.Find("Torch2").gameObject.SetActive(true);
+            transform.parent.Find("Steam").gameObject.SetActive(true);
         }
     }
 
