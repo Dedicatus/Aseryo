463a95d6fbeb90f8be5afc047edac94e7576bbfc
Yiming Zhang <allenzym@gmail.com>
2019-12-10 20:59:21 -0800

    trigger timer ui

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    public GameObject skillSelect, PlayerStatus, gameOver;
+    public GameObject skillSelect, PlayerStatus, gameOver, Timer;
+    bool timerActive;
+    float timeCount;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,11 +18,35 @@ public class UIManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (timerActive)
+        {
+            timeCount -= Time.deltaTime;
+            Timer.GetComponent<Text>().text = Mathf.RoundToInt(timeCount).ToString();
+
+            if (timeCount <= 0)
+            {
+                hideTimer();
+                Timer.SetActive(false);
+                timerActive = false;
+            }
+        }
     }
 
     public void showGameOver()
     {
         gameOver.SetActive(true);
+    }
+
+    public void showTimer(float time)
+    {
+        Timer.SetActive(true);
+        timerActive = true;
+        timeCount = time;
+    }
+
+    void hideTimer()
+    {
+        Timer.SetActive(true);
+        timerActive = true;
     }
 }
