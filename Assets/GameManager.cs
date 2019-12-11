ddbbc864a6b91201915d1f17cb49bccee597712d
huyiqun753 <56007804+huyiqun753@users.noreply.github.com>
2019-12-10 18:10:23 -0800

    player death animation update

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,21 +5,36 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameObject UIManager;
+    public float waitTime = 2f;
+    float waitTimeCount;
     // Start is called before the first frame update
     void Start()
     {
-        
+        waitTimeCount = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        countGameOver();
+    }
+
+    void countGameOver()
+    {
+        if (waitTimeCount > 0)
+        {
+            waitTimeCount -= Time.deltaTime;
+            if (waitTimeCount <= 0)
+            {
+                Time.timeScale = 0;
+                UIManager.GetComponent<UIManager>().showGameOver();
+            }
+        }
+
     }
 
     public void gameOver()
     {
-        Time.timeScale = 0;
-        UIManager.GetComponent<UIManager>().showGameOver();
+        waitTimeCount = waitTime;
     }
 }
